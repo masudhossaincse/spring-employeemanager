0fb82c78e372de1054d45d472abbb0fe3e9c2162
masud <masud@eatlbd.com>
2021-03-07 11:59:58 +0600

    Feature CRUD Rest Api complete

--- src/main/java/com/masud/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/masud/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.masud.employeemanager.repo;
 import com.masud.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
